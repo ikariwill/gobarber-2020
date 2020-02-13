b6177fc685b18fa13a6fb401f0153e370bf1c556
Willian Home <willian@wiki4fit.com.br>
2020-02-13 20:57:02 -0300

    upload de arquivos

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 
 import UserController from './app/controllers/UserController';
+import ProviderController from './app/controllers/ProviderController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
-import auth from './app/middlewares/auth';
+import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 // SESSIONS
 routes.post('/sessions', SessionController.store);
@@ -13,10 +18,20 @@ routes.post('/sessions', SessionController.store);
 // USERS
 routes.post('/users', UserController.store);
 
-routes.put('/users', auth, UserController.update);
+routes.put('/users', authMiddleware, UserController.update);
 
 routes.get('/users', UserController.index);
 
 routes.get('/users/:email', UserController.show);
+
+routes.get('/providers', authMiddleware, ProviderController.index);
+
+// UPLOAD
+routes.post(
+  '/files',
+  authMiddleware,
+  upload.single('file'),
+  FileController.store
+);
 
 export default routes;
