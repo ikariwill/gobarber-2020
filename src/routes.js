7520ea840dd86659137237df1c0eda9c806f05d0
Willian Home <willian@wiki4fit.com.br>
2020-02-14 19:17:39 -0300

    listagem de horários disponíveis

--- src/routes.js
+++ src/routes.js
@@ -9,16 +9,21 @@ import FileController from './app/controllers/FileController';
 import AppointmentController from './app/controllers/AppointmentController';
 import ScheduleController from './app/controllers/ScheduleController';
 import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import loggedOnly from './app/middlewares/auth';
 
 const routes = new Router();
 const upload = multer(multerConfig);
 
-// SESSIONS
+/**
+ * SESSIONS
+ */
 routes.post('/sessions', SessionController.store);
 
-// USERS
+/**
+ * USERS
+ */
 routes.post('/users', UserController.store);
 
 routes.put('/users', loggedOnly, UserController.update);
@@ -27,18 +32,37 @@ routes.get('/users', UserController.index);
 
 routes.get('/users/:email', UserController.show);
 
+/**
+ * PROVIDERS
+ */
 routes.get('/providers', loggedOnly, ProviderController.index);
+routes.get(
+  '/providers/:providerId/available',
+  loggedOnly,
+  AvailableController.index
+);
 
+/**
+ * APPOINTMENTS
+ */
 routes.post('/appointments', loggedOnly, AppointmentController.store);
 routes.get('/appointments', loggedOnly, AppointmentController.index);
 routes.delete('/appointments/:id', loggedOnly, AppointmentController.destroy);
 
+/**
+ * SCHEDULES
+ */
 routes.get('/schedules', loggedOnly, ScheduleController.index);
 
+/**
+ * NOTIFICATIONS
+ */
 routes.get('/notifications', loggedOnly, NotificationController.index);
 routes.put('/notifications/:id', loggedOnly, NotificationController.update);
 
-// UPLOAD
+/**
+ * FILES
+ */
 routes.post('/files', loggedOnly, upload.single('file'), FileController.store);
 
 export default routes;
